ecb392bd3e281e482050efb64f77dbb3ccbeca2e
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-03-05 13:43:09 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide(){
+    let num1=2000;
+    let num2 =100;
+return num1/num2
+
+}
+const square =()=>{
+    let num1 =2
+    let num2=num1
+    return num1*num2
+}
+const add =()=>{
+    let num1=5
+    let num2=2
+    return num1+ num2
+}
